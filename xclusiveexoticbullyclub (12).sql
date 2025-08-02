246a68cc169312c090aba8872639eac0c7ec53fb
JARP <105678913+anderson895@users.noreply.github.com>
2025-08-02 16:14:31 +0800

    add banner, fblink and ig link

--- xclusiveexoticbullyclub (12).sql
+++ xclusiveexoticbullyclub (12).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 02, 2025 at 08:10 AM
+-- Generation Time: Aug 02, 2025 at 10:14 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -51,17 +51,20 @@ CREATE TABLE `dogs` (
   `dog_id` int(11) NOT NULL,
   `dog_code` varchar(60) NOT NULL,
   `dog_name` varchar(60) NOT NULL,
+  `dog_date_registration` date DEFAULT NULL,
   `dog_owner_name` varchar(60) DEFAULT NULL,
   `dog_breeder_name` varchar(60) DEFAULT NULL,
-  `dog_date_registration` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `dog_image` varchar(255) NOT NULL,
+  `dog_banner` varchar(255) DEFAULT NULL,
   `dog_country` varchar(60) DEFAULT NULL,
   `dog_color` varchar(60) DEFAULT NULL,
   `dog_height` varchar(60) DEFAULT NULL,
   `dog_date_of_birth` date DEFAULT NULL,
   `dog_contact_number` varchar(25) DEFAULT NULL,
   `dog_facebook_name` varchar(60) DEFAULT NULL,
+  `dog_facebook_link` varchar(255) DEFAULT NULL,
   `dog_ig_name` varchar(60) DEFAULT NULL,
+  `dog_ig_link` varchar(255) DEFAULT NULL,
   `dog_type_status` varchar(60) DEFAULT NULL,
   `dog_registered_status` int(11) NOT NULL COMMENT '0=not registered,1=registered,2=archived'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -70,30 +73,8 @@ CREATE TABLE `dogs` (
 -- Dumping data for table `dogs`
 --
 
-INSERT INTO `dogs` (`dog_id`, `dog_code`, `dog_name`, `dog_owner_name`, `dog_breeder_name`, `dog_date_registration`, `dog_image`, `dog_country`, `dog_color`, `dog_height`, `dog_date_of_birth`, `dog_contact_number`, `dog_facebook_name`, `dog_ig_name`, `dog_type_status`, `dog_registered_status`) VALUES
-(13, '99000004791297', 'husky', 'april jane', 'juan dela cruz', '2025-07-31 07:20:54', 'dog_688b19568fceb0.96194899.png', 'philippines', 'white', '3ft', '2025-03-25', '09454454744', 'HUBBY', 'HUBBY', 'regular', 1),
-(14, '99000007215398', 'alpha', 'joshua padilla', 'juan dela cruz', '2025-07-31 17:52:31', 'dog_688b9003634a94.68791231.jpg', 'philippines', 'white', '5ft', '2025-03-25', '09454454744', 'booby', 'booby', 'exclusive', 1),
-(17, '99000003939935', 'mark', NULL, NULL, '2025-07-31 15:16:06', 'dog_688b88b6d6ac99.27833191.jpg', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
-(18, '99000001784755', 'fdefsefse', NULL, NULL, '2025-07-31 15:35:47', 'dog_688b8d535bb9f9.03702900.jpg', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
-(19, '99000007402568', 'patty', NULL, NULL, '2025-07-31 16:04:33', 'dog_688b9411105315.87173173.webp', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
-(20, '99000002157671', 'master', NULL, NULL, '2025-07-31 16:15:27', 'dog_688b969f650a01.66399990.webp', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
-(21, '99000001510201', 'Bella', 'bella padilla', 'justin bieber', '2025-07-31 16:16:27', 'dog_688b96db600411.97613913.avif', 'USA', 'gray', '15 cm', '2025-08-01', '09454454744', 'bella', 'bella', 'regular', 1),
-(22, '99000003820257', 'batman', 'robin padilla', 'justin bieber', '2025-07-31 17:52:21', 'dog_688b988a0fafa4.22209273.avif', 'philippines', 'black and white', '4 fit', '2025-08-01', '09454454744', 'BATMAN', 'BATMAN', 'exclusive', 1),
-(23, '99000000110469', 'koko', NULL, NULL, '2025-07-31 16:24:09', 'dog_688b98a9897801.96242836.jpg', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
-(24, '99000006610296', 'mac mac', NULL, NULL, '2025-07-31 16:24:29', 'dog_688b98bdc0d2d5.07642578.jpg', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
-(25, '99000004426855', 'tan tan', NULL, NULL, '2025-07-31 16:24:44', 'dog_688b98ccaf70a2.97428477.webp', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
-(26, '99000005246561', 'kiko', NULL, NULL, '2025-07-31 16:25:02', 'dog_688b98de6bcbc5.58173858.webp', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
-(27, '99000008218255', 'chubby', 'juan dela cruz', 'joshua padilla', '2025-07-31 17:29:03', 'dog_688ba14702f6b2.11945236.avif', 'philippines', 'brown', '4ft', '2025-08-01', '09454454744', 'chubby', 'chubby', 'exclusive', 1),
-(28, '99000009133247', 'alucard', NULL, NULL, '2025-07-31 17:01:18', 'dog_688ba15e961a01.25065870.webp', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
-(29, '99000007531350', 'lebron', NULL, NULL, '2025-07-31 17:02:02', 'dog_688ba18a830450.52300802.webp', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
-(30, '99000003461104', 'chuchay', 'april jane', 'joshua padilla', '2025-08-02 01:06:38', 'dog_688ba8ca1123b8.27562269.jpg', 'philippines', 'brown', '15cm', '2025-08-01', '09454454744', 'chuchay', 'chuchay', 'exclusive', 2),
-(31, '99000001989421', 'awdaw', 'DWAD', 'GREG', '2025-07-31 17:34:05', 'dog_688ba90d6fdb41.19073794.jpg', 'TRH', 'R5H', 'TJ7', '2025-08-20', 'AWAW', '2AD', 'AWAWDWAD', 'regular', 1),
-(33, '99000003223376', 'bobby', 'bobby', 'bobby', '2025-07-31 17:39:12', 'dog_688baa409f2a58.66027668.webp', 'bobby', 'bobby', 'bobby', '2025-08-01', '09454454744', 'awdawd', 'awdwa', 'exclusive', 1),
-(34, '99000004859503', 'paemla', NULL, NULL, '2025-07-31 18:40:47', 'dog_688bb8af0e4f45.83773736.avif', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
-(35, '99000003571079', 'kokey', NULL, NULL, '2025-07-31 18:41:05', 'dog_688bb8c155cd86.35200608.webp', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
-(36, '99000001553573', 'jun jun', NULL, NULL, '2025-07-31 18:41:24', 'dog_688bb8d43d7206.87002647.avif', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
-(37, '99000006248874', 'test', NULL, NULL, '2025-07-31 18:41:38', 'dog_688bb8e228ac89.54008414.avif', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
-(38, '99000008582419', 'kong kong', NULL, NULL, '2025-08-02 03:52:15', 'dog_688d8b6fc67123.47070007.webp', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0);
+INSERT INTO `dogs` (`dog_id`, `dog_code`, `dog_name`, `dog_date_registration`, `dog_owner_name`, `dog_breeder_name`, `dog_image`, `dog_banner`, `dog_country`, `dog_color`, `dog_height`, `dog_date_of_birth`, `dog_contact_number`, `dog_facebook_name`, `dog_facebook_link`, `dog_ig_name`, `dog_ig_link`, `dog_type_status`, `dog_registered_status`) VALUES
+(41, '99000005766653', 'TBC MIAGI', NULL, 'JORGE SOTO', 'JORGE SOTO', 'dog_688dc85bb80d92.63592581.jpg', 'banner_688dc8888f9891.70421033.jpg', 'USA', 'blue', 'N/A', '2011-01-18', '09999', 'JORGE LUIS SOTO', 'https://www.facebook.com/jorge.luis.soto.59319?mibextid=wwXIfr&rdid=P1FmWcJE1YdwOeIj&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F19KcYmyrS3%2F%3Fmibextid%3DwwXIfr#', 'thebullycampline', 'https://www.instagram.com/thebullycampline/', 'exclusive', 1);
 
 -- --------------------------------------------------------
 
@@ -125,14 +106,7 @@ CREATE TABLE `generation` (
 --
 
 INSERT INTO `generation` (`gen_id`, `gen_dog_id`, `father_dog_id`, `mother_dog_id`, `grandfather1_dog_id`, `grandmother1_dog_id`, `grandfather2_dog_id`, `grandmother2_dog_id`, `ggfather1_dog_id`, `ggmother1_dog_id`, `ggfather2_dog_id`, `ggmother2_dog_id`, `ggfather3_dog_id`, `ggmother3_dog_id`, `ggfather4_dog_id`, `ggmother4_dog_id`) VALUES
-(3, 13, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 14, 20),
-(4, 14, NULL, NULL, NULL, 38, 27, NULL, 19, NULL, NULL, NULL, NULL, NULL, 13, 18),
-(8, 21, 22, 13, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(9, 22, 21, 23, 24, 35, 26, 34, 31, 30, 13, 27, 36, 33, 37, 14),
-(10, 27, 28, 13, 21, 22, 29, 14, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(11, 30, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(12, 31, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(14, 33, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+(17, 41, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -147,17 +121,6 @@ CREATE TABLE `pageant` (
   `pag_date_added` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `pageant`
---
-
-INSERT INTO `pageant` (`pag_id`, `pag_name`, `pag_description`, `pag_date_added`) VALUES
-(1, 'test', 'descr', '2025-08-01 08:09:54'),
-(2, 'new', 'awdaw', '2025-08-01 08:11:06'),
-(3, 'jjjjjjjjjjjj', 'dddddddd', '2025-08-01 08:12:24'),
-(4, 'dog show 2025', 'awdaw', '2025-08-01 08:38:53'),
-(5, 'dog show 22222', 'awdwad', '2025-08-01 08:49:00');
-
 -- --------------------------------------------------------
 
 --
@@ -170,13 +133,6 @@ CREATE TABLE `pageant_category` (
   `pc_category_name` text NOT NULL,
   `pc_contestant` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT 'list all the id of contestant here in array format'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `pageant_category`
---
-
-INSERT INTO `pageant_category` (`pc_id`, `pc_pageant_id`, `pc_category_name`, `pc_contestant`) VALUES
-(20, 5, 'talent', '[{\"id\":\"27\",\"points\":\"60\"},{\"id\":\"22\",\"points\":\"40\"},{\"id\":\"14\",\"points\":\"2\"}]');
 
 --
 -- Indexes for dumped tables
@@ -241,13 +197,13 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `dogs`
 --
 ALTER TABLE `dogs`
-  MODIFY `dog_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `dog_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `generation`
 --
 ALTER TABLE `generation`
-  MODIFY `gen_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `gen_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `pageant`
@@ -259,7 +215,7 @@ ALTER TABLE `pageant`
 -- AUTO_INCREMENT for table `pageant_category`
 --
 ALTER TABLE `pageant_category`
-  MODIFY `pc_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `pc_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Constraints for dumped tables
