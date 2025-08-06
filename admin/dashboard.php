b7e85f5098cf565d49c296b2d297d223a53e9565
JARP <105678913+anderson895@users.noreply.github.com>
2025-08-06 18:16:29 +0800

    remove not registered

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -25,10 +25,10 @@ include "../src/components/admin/nav.php";
     <p class="text-[#CCCCCC] text-sm">Total Registered</p>
     <h2 class="text-3xl font-bold text-[#FFD700]" id="totalRegistered">0</h2>
   </div>
-  <div class="bg-[#1A1A1A] p-4 rounded-lg shadow text-center border border-[#333]">
+  <!-- <div class="bg-[#1A1A1A] p-4 rounded-lg shadow text-center border border-[#333]">
     <p class="text-[#CCCCCC] text-sm">Total Not Registered</p>
     <h2 class="text-3xl font-bold text-[#FFD700]" id="totalNotRegistered">0</h2>
-  </div>
+  </div> -->
   <div class="bg-[#1A1A1A] p-4 rounded-lg shadow text-center border border-[#333]">
     <p class="text-[#CCCCCC] text-sm">Total Gettable</p>
     <h2 class="text-3xl font-bold text-[#FFD700]" id="totalGettable">0</h2>
